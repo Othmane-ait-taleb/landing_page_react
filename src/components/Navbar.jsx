1bd723e89d3eb0508d3c86c27d0f9b9bb61c330e
othmane ait taleb <aittaleb108@gmail.com>
2022-11-27 16:30:38 +0100

    adding nav bar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {TiThMenuOutline } from 'react-icons/ti'
+function Navbar() {
+  return (
+    <div className='w-screen h-[80px]  bg-gray-600 shadow-md shadow-black z-10 '>
+        <TiThMenuOutline className='text-white '/>
+    </div>
+  )
+}
+
+export default Navbar
